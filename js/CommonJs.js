0816c2cfae72ebd30abc545167019448bc2b9ff6
Vidya Niwas Pandey <vidyaniwas2@gmail.com>
2025-01-10 23:19:26 +0530

    ABC Changes

--- js/CommonJs.js
+++ js/CommonJs.js
@@ -1,27 +1,27 @@
 window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'UA-153937526-1');
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+gtag('config', 'UA-153937526-1');
 
-	// Function to calculate experience
-		function calculateExperience() {
-		    const startDate = new Date("2022-05-02"); // May 2, 2022.
-		    const currentDate = new Date();
-		
-		    // Calculate the difference in time (in milliseconds)
-		    const timeDifference = currentDate - startDate;
-		
-		    // Convert time difference to years
-		    const experienceInYears = timeDifference / (1000 * 3600 * 24 * 365);
-		
-		    // Round the result to 1 decimal point
-		    const roundedExperience = experienceInYears.toFixed(1);
-		
-		    // Display the experience in the HTML element.
-		    document.getElementById("experience").innerText = roundedExperience;
-		}
-		// Call the function to calculate experience on page load
-		window.onload = calculateExperience;
+// Function to calculate experience
+function calculateExperience() {
+    const startDate = new Date("2022-05-02"); // May 2, 2022.
+    const currentDate = new Date();
+
+    // Calculate the difference in time (in milliseconds)
+    const timeDifference = currentDate - startDate;
+
+    // Convert time difference to years
+    const experienceInYears = timeDifference / (1000 * 3600 * 24 * 365);
+
+    // Round the result to 1 decimal point
+    const roundedExperience = experienceInYears.toFixed(1);
+
+    // Display the experience in the HTML element.
+    document.getElementById("experience").innerText = roundedExperience;
+}
+// Call the function to calculate experience on page load
+window.onload = calculateExperience;
 
 
 function validateForm() {
@@ -29,7 +29,7 @@ function validateForm() {
     let message = document.getElementById("contactMessage").value.trim();
     let emailError = document.getElementById("emailError");
     let messageError = document.getElementById("messageError");
- 
+
     let isValid = true;
 
     // Reset error messages
